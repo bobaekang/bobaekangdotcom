e9a147941107a7ef8877cd2f34cf833e6f247653
bobaekang <bkang.dev@gmail.com>
2024-06-20 18:46:56 -0500

    Fix rss setup

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,15 +2,17 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
-export async function get(context) {
+export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      link: `/blog/${post.slug}/`,
-    })),
+    items: posts
+      .map((post) => ({
+        ...post.data,
+        link: `/blog/${post.slug}/`,
+      }))
+      .sort((a, b) => a.pubDate - b.pubDate),
   });
 }

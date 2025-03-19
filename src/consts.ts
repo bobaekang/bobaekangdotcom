4dfc1177f0153250980d8d8da74601eff1cf1460
bobaekang <bkang.dev@gmail.com>
2025-03-19 11:12:33 -0500

    Update site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "bobae kang";
-export const SITE_DESCRIPTION = "Social scientist turned software engineer";
+export const SITE_DESCRIPTION =
+  "Full-stack product engineer building quality & impactful software";

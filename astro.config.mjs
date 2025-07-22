4d738ee190c031fa52d0190c205fe93e80ab22c2
bobaekang <bkang.dev@gmail.com>
2025-07-22 16:18:41 -0500

    Migrate tailwind to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import icon from 'astro-icon'
+import { defineConfig } from 'astro/config'
+
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bobaekang.com",
-  integrations: [mdx(), sitemap(), tailwind(), icon()],
-});
+  site: 'https://bobaekang.com',
+  integrations: [mdx(), sitemap(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})

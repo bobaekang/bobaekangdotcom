00ea430f51eb2a0abd0c8a350d1bee695f306abb
bobaekang <bkang.dev@gmail.com>
2026-02-15 12:01:09 -0600

    Switch to built-in node --env-files

--- fetchBlogSource.js
+++ fetchBlogSource.js
@@ -1,15 +1,12 @@
-import dotenv from "dotenv";
-import fs from "fs";
-import path from "path";
-import stream from "stream";
-import url from "url";
-import util from "util";
+import fs from "fs"
+import path from "path"
+import stream from "stream"
+import url from "url"
+import util from "util"
 
-dotenv.config();
-
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const asyncPipeline = util.promisify(stream.pipeline);
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const asyncPipeline = util.promisify(stream.pipeline)
 
 const CONFIG = {
   posts: {
@@ -21,20 +18,20 @@ const CONFIG = {
     url: `${process.env.BLOG_SRC_URL}/assets/images`,
     dirname: "/public/images/blog",
   },
-};
+}
 
 /** @param {string} url */
 async function fetchFromGitHub(url) {
   try {
     const res = await fetch(url, {
       headers: { Authorization: `token ${process.env.GITHUB_TOKEN}` },
-    });
+    })
 
-    if (res.ok) return res;
-    throw new Error(`Failed to fetch from ${url} with status ${res.status}`);
+    if (res.ok) return res
+    throw new Error(`Failed to fetch from ${url} with status ${res.status}`)
   } catch (e) {
-    console.error("fetchFromGitHub", e);
-    throw e;
+    console.error("fetchFromGitHub", e)
+    throw e
   }
 }
 
@@ -44,17 +41,17 @@ async function fetchFromGitHub(url) {
  * @param {string} filename
  */
 async function writeBlogPost(res, dirPath, filename) {
-  const [date, updatedFilename] = filename.split("_");
-  const filePath = path.join(dirPath, updatedFilename);
-  const pubDate = `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6)}`;
+  const [date, updatedFilename] = filename.split("_")
+  const filePath = path.join(dirPath, updatedFilename)
+  const pubDate = `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6)}`
   try {
-    const text = await res.text();
-    const [firstLine, ...lines] = text.split("\n");
-    const pubDateLine = `pubDate: "${pubDate}Z-06:00"`; // use central time zone
-    const updatedText = [firstLine, pubDateLine, ...lines].join("\n");
-    fs.promises.writeFile(filePath, updatedText);
+    const text = await res.text()
+    const [firstLine, ...lines] = text.split("\n")
+    const pubDateLine = `pubDate: "${pubDate}Z-06:00"` // use central time zone
+    const updatedText = [firstLine, pubDateLine, ...lines].join("\n")
+    fs.promises.writeFile(filePath, updatedText)
   } catch (e) {
-    console.error("writeBlogPost", e);
+    console.error("writeBlogPost", e)
   }
 }
 
@@ -64,28 +61,28 @@ async function writeBlogPost(res, dirPath, filename) {
  * @param {string} filename
  */
 async function writeBlogFile(res, dirPath, filename) {
-  const filePath = path.join(dirPath, filename);
-  asyncPipeline(res.body, fs.createWriteStream(filePath));
+  const filePath = path.join(dirPath, filename)
+  asyncPipeline(res.body, fs.createWriteStream(filePath))
 }
 
 /** @param {{ url: string, dirname: string }} config */
 async function fetchBlogSource(config) {
-  const dirPath = path.join(__dirname, config.dirname);
-  if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath, { recursive: true });
+  const dirPath = path.join(__dirname, config.dirname)
+  if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath, { recursive: true })
 
   try {
-    const res = await fetchFromGitHub(config.url);
-    const files = await res.json();
+    const res = await fetchFromGitHub(config.url)
+    const files = await res.json()
     for (const file of files) {
-      const res = await fetchFromGitHub(file.download_url);
-      if (config.isPost) writeBlogPost(res, dirPath, file.name);
-      else writeBlogFile(res, dirPath, file.name);
+      const res = await fetchFromGitHub(file.download_url)
+      if (config.isPost) writeBlogPost(res, dirPath, file.name)
+      else writeBlogFile(res, dirPath, file.name)
     }
   } catch (e) {
-    console.error("fetchBlogSource", e);
-    process.exit(1);
+    console.error("fetchBlogSource", e)
+    process.exit(1)
   }
 }
 
-console.log("Fetch blog source...");
-Object.values(CONFIG).map(fetchBlogSource);
+console.log("Fetch blog source...")
+Object.values(CONFIG).map(fetchBlogSource)
